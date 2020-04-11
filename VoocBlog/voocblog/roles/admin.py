3005f23ca5a18d90cd74be5e902bad103b58ceeb
haitac1998 <51694793+haitac1998@users.noreply.github.com>
2020-04-11 22:13:54 +0700

    SQL
    Add Blog/Posts

--- VoocBlog/voocblog/roles/admin.py
+++ VoocBlog/voocblog/roles/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Post
 # Register your models here.
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title_post', 'pub_date_post']
+    list_filter = ['pub_date_post']
+    search_fields = ['id']
+admin.site.register(Post, PostAdmin)
