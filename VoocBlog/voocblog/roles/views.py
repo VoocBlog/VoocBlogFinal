660139ad50261d3f374cb65e5476e0aea8d50e07
phatntt1999 <phatntt1999@gmail.com>
2020-04-29 12:13:22 +0700

    fix bug front-end

--- VoocBlog/voocblog/roles/views.py
+++ VoocBlog/voocblog/roles/views.py
@@ -4,6 +4,8 @@ from .forms import PostForm
 from django import forms
 
 from .models import Post
+
+from django.http import HttpResponse
 # Create your views here.
 
 #def post_create_view(request):
@@ -25,6 +27,12 @@ from .models import Post
     #         return HttpResponseRedirect('/')
     # return render( request, "blog/blog_create.html", {'form' : form} )
 
+# def IsInSession(request):
+#     if request.session.has_key('username'):
+#         return HttpResponseRedirect('posting-post')
+#     else:
+#         return HttpResponse('Must login')
+        
 def post_create_view(request):
     context = {
         "form":PostForm
